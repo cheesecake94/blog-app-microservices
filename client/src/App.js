90ae1a4021aa3fadc5fdff33729526032d963bfb
Amine <aminefilali@outlook.dk>
2021-06-19 09:06:37 +0200

    finishing up client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import CreatePost from './CreatePost';
+import PostList from './PostList';
 
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className="container">
+      <h1>Create post</h1>
+      <CreatePost />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // In-memory posts
 const posts = {};
@@ -16,7 +18,7 @@ app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const title = req.body.title;
 
-  posts[id] = { title }
+  posts[id] = { id, title }
 
   res.status(201).send(posts[id]);
 });
