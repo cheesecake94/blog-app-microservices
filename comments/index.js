7beac1b8f62b9abc4b793b3e6f9bd2bf61027256
Amine <aminefilali@outlook.dk>
2021-06-19 10:15:38 +0200

    event bus + query service added

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[postId]);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const content = req.body.content;
   const postId = req.params.id;
@@ -26,7 +27,16 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[postId] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: { id, content, postId }
+  });
+
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log(`Event received [${req.body.type}]`);
 });
 
 app.listen(4001, () => {
